264e99c9e298ae9bf92030368e473ed10eb0a5b7
Prajna <prajnapprabhu3@gmail.com>
2024-07-03 15:42:22 +0530

    vcs.xml file changes

--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 
-public class UserBookingService{
+public class git addUserBookingService{
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
